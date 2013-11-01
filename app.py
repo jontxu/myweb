e9df7f899c9226335da85393b5e04c2f3485bea9
jontxu <jon.jc22@gmail.com>
2013-11-01 01:20:36 +0100

    It should work

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def contact():
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0')
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', port=port)
